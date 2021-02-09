774095b4baebabadfd3f1e37091f1731492889c0
retyper <retyper@naver.com>
2021-02-10 08:04:53 +0900

    000webhost 용 db.php 변경 다시

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 $conn = mysqli_connect(
- 'id16120848_localhost',
- 'id16120848_root',
- '}gCl!{nT80@TFdOS',
- 'id16120848_localhost')
- or die ("connect fail");
+  'localhost',
+  'id16120848_root',
+  '}gCl!{nT80@TFdOS',
+  'id16120848_localhost')
+  or die ("connect fail");
 
 session_start();
 ?>
