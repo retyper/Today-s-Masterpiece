3b91d478db7050829ebe0d6096b9190c494860c5
retyper <retyper@naver.com>
2021-02-16 21:18:23 +0900

    service_like 테이블 수정

--- like_proc.php
+++ like_proc.php
@@ -5,18 +5,18 @@ if(!isset($_SESSION["userid"]))
 header("location:login.php");
 }
 
-$id = $id = $_SESSION['userid']; // 사용자의 IP주소 가져오기
+$id = $id = $_SESSION['userid'];
 $impression_id = $_POST['articleId']; // 게시글 아이디
 $service_code = $_GET['getLikedByCode'];
 
 if(!empty($impression_id)) {
-    $sql1 = "SELECT * from service_like WHERE service_code = 'phpex-$impression_id' AND liked_audience = '$id'";
+    $sql1 = "SELECT * from service_like WHERE service_code = '$impression_id' AND liked_audience = '$id'";
     $result1 = mysqli_query($conn,$sql1);
     $res1 = mysqli_num_rows($result1);
 
     if($res1 == 0) {
         // 좋아요 기록이 없는 경우 -> 좋아요 등록
-        $sql2 = "insert into service_like(service_code, liked_audience, date) VALUES('phpex-$impression_id', '$id', NOW())";
+        $sql2 = "insert into service_like(service_code, liked_audience, date) VALUES('$impression_id', '$id', NOW())";
         $res2 = mysqli_query($conn,$sql2);
 
         // 게시판 테이블 업데이트
@@ -25,7 +25,7 @@ if(!empty($impression_id)) {
         echo $res2 && $res3 ? "like" : "failed";
     } else {
         // 이미 좋아요를 누른 경우 -> 좋아요 취소
-        $sql2 = "DELETE from service_like WHERE service_code = 'phpex-$impression_id' AND liked_audience = '$id'";
+        $sql2 = "DELETE from service_like WHERE service_code = '$impression_id' AND liked_audience = '$id'";
         $res2 = mysqli_query($conn,$sql2);
 
         // 게시판 테이블 업데이트
@@ -34,7 +34,7 @@ if(!empty($impression_id)) {
         echo $res2 && $res3 ? "unlike" : "failed";
     }
 } else if(!empty($service_code)) {
-    $sql1 = "SELECT * from service_like WHERE service_code = 'phpex-$service_code' AND liked_audience = '$id'";
+    $sql1 = "SELECT * from service_like WHERE service_code = '$service_code' AND liked_audience = '$id'";
     $result1 = mysqli_query($conn,$sql1);
     $res1 = mysqli_num_rows($result1);
 
